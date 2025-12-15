3f267db1c94041ceb345dcb4fd714e09018e37ef
Dakota <csmith1865@users.noreply.github.com>
2025-12-15 12:38:29 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ function noSearchDefaultPageRender() {
   app.innerHTML = `
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
       <div class="content-container">
-        <h1>Und*ck</h1>
+        <h1>Und*ck Docker</h1>
         <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a></p>
         <div class="url-container"> 
           <input 
             type="text" 
             class="url-input"
-            value="https://unduck.link?q=%s"
+            value="https://unduck.sumisu.xyz?q=%s"
             readonly 
           />
           <button class="copy-button">
@@ -25,7 +25,9 @@ function noSearchDefaultPageRender() {
         •
         <a href="https://x.com/theo" target="_blank">theo</a>
         •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
+        <a href="https://github.com/t3dotgg/unduck" target="_blank">original github</a>
+        •
+        <a href="https://github.com/csmith1865/unduck-docker" target="_blank">github</a>
       </footer>
     </div>
   `;
